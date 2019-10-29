f2ed737cd1e5c19d8392667ed01322653a851807
gaolu11 <768065158@qq.com>
2019-10-29 10:11:57 +0800

    feat: 优化 sketchJsonToSketchFile 方法

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const { Sketch2json, readAndParseFileInSketch } = require('./lib/index.js')
+const { Sketch2json, readAndParseFileInSketch, sketchJsonToSketchFile } = require('./lib/index.js')
 const sketch2json = require('sketch2json')
 const fs = require('fs')
 const fse = require('fs-extra')
@@ -7,11 +7,22 @@ const fse = require('fs-extra')
 
 // 解压 sketch file https://github.com/EvanOxfeld/node-unzip
 // Sketch2json('./sketch-examples/test.sketch', {
-//   single: false,
+//   single: true,
 //   output: `${__dirname}/dist/`
 // }).then(() => {
 //   console.error('Sketch2json 解压 sketch file 完成！！')
 // })
 
 
-readAndParseFileInSketch('./output/')
+// readAndParseFileInSketch 读取 sketch 解压文件路径生成 sketch 文件
+// readAndParseFileInSketch('./output/')
+
+
+// sketchJsonToSketchFile 根据sketch json 数据生成 sketch 文件
+const singleSketchJson = fse.readJsonSync('./test-json/single-sketch.json')
+const preview = fse.readFile('./test-json/preview.png')
+const images = [{
+  '38ca6125035f10996026c761d9779f85c8d1f99e': fse.readFile('./test-json/38ca6125035f10996026c761d9779f85c8d1f99e.png')
+}]
+const output = './single.sketch'
+sketchJsonToSketchFile({ singleSketchJson, preview, images }, output)
