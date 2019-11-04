00bf44ad123f966f268ce92d4960f19f59a680dd
gaolu11 <768065158@qq.com>
2019-11-04 17:31:54 +0800

    feat: 扩展方法

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const { Sketch2json, readAndParseFileInSketch, sketchJsonToSketchFile } = require('./lib/index.js')
+const { Sketch2json, readAndParseFileInSketch, sketchJsonToSketchFile, multiplArtboardJsonToSketchFile } = require('./lib/index.js')
 const fs = require('fs')
 const fse = require('fs-extra')
 // Document Meta User Page Image Preview
@@ -18,10 +18,17 @@ const fse = require('fs-extra')
 
 
 // sketchJsonToSketchFile 根据sketch json 数据生成 sketch 文件
-const singleSketchJson = fse.readJsonSync('./test-json/single-sketch.json')
-const preview = fse.readFile('./test-json/preview.png')
-const images = [{
-  '38ca6125035f10996026c761d9779f85c8d1f99e.png': fse.readFile('./test-json/38ca6125035f10996026c761d9779f85c8d1f99e.png')
-}]
-const output = './single.sketch'
-sketchJsonToSketchFile({ sketchJson: singleSketchJson, preview, images }, output)
+// const singleSketchJson = fse.readJsonSync('./test-json/single-sketch.json')
+// const preview = fse.readFile('./test-json/preview.png')
+// const images = [{
+//   '38ca6125035f10996026c761d9779f85c8d1f99e.png': fse.readFile('./test-json/38ca6125035f10996026c761d9779f85c8d1f99e.png')
+// }]
+// const output = './single.sketch'
+// sketchJsonToSketchFile({ sketchJson: singleSketchJson, preview, images }, output)
+
+// 3c959080-f72b-11e9-8aed-f77013a4e0b0.json
+const artboard = fse.readJsonSync('./test-json/3c959080-f72b-11e9-8aed-f77013a4e0b0.json')
+const artboard1 = fse.readJsonSync('./test-json/06acbf70-f564-11e9-a6c2-fd57ed176182.json')
+multiplArtboardJsonToSketchFile([artboard, artboard1], './single.sketch')
+
+
