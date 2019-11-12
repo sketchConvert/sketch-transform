6f16c8fa915164132091eb589aface965bac840c
gaolu11 <768065158@qq.com>
2019-11-12 19:14:38 +0800

    feat: compile base64 to local Image

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 
 const { Sketch2json, readAndParseFileInSketch, sketchJsonToSketchFile, multiplArtboardJsonToSketchFile } = require('./lib/index.js')
 const fs = require('fs')
+const path = require('path')
 const fse = require('fs-extra')
+const axios = require('axios')
+const { dirSync, fileSync } = require('tmp')
 // Document Meta User Page Image Preview
 
 // 解压 sketch file https://github.com/EvanOxfeld/node-unzip
@@ -29,6 +32,42 @@ const fse = require('fs-extra')
 // 3c959080-f72b-11e9-8aed-f77013a4e0b0.json
 const artboard = fse.readJsonSync('./test-json/3c959080-f72b-11e9-8aed-f77013a4e0b0.json')
 const artboard1 = fse.readJsonSync('./test-json/06acbf70-f564-11e9-a6c2-fd57ed176182.json')
-multiplArtboardJsonToSketchFile([artboard, artboard1], './single.sketch')
 
 
+const getArtboardsJson = async (urls = []) => {
+  const requests = []
+  urls.map(url => {
+    const getJson = new Promise(resolve => {
+      axios.get(url)
+        .then(function (response) {
+          // handle success
+          resolve(response.data)
+        })
+        .catch(() => {
+          resolve('')
+        })
+    })
+
+    requests.push(getJson)
+  })
+
+  const result = await Promise.all(requests)
+  return result
+}
+
+
+const artboardsJsonTransfromSketch = async urls => {
+  const artboards = await getArtboardsJson(urls)
+
+  const tmpFile = fileSync({ postfix: '.sketch', prefix: 'sketch_' })
+  // console.error('tmpFile', tmpFile)
+  await multiplArtboardJsonToSketchFile(artboards, './single.sketch')
+  // console.error('fs.statSync(tmpFile)', fs.statSync(tmpFile.name))
+  tmpFile.removeCallback()
+}
+
+
+artboardsJsonTransfromSketch([
+  'http://storage.360buyimg.com/quark-platform/design/06acbf70-f564-11e9-a6c2-fd57ed176182.json',
+  'http://storage.360buyimg.com/quark-platform/design/3c959080-f72b-11e9-8aed-f77013a4e0b0.json'
+])
